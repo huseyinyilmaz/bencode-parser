dcef9b6cc5c88160c4071cbfb8b3f48a4327548c
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2016-05-31 10:02:57 -0700

    Add dictonary parser and add decode function.

--- bencode.py
+++ bencode.py
@@ -1,38 +1,100 @@
-from pyparsing import Literal,CaselessLiteral,Word,Combine,Group,Optional,\
-    ZeroOrMore,Forward,nums,alphas,Suppress,printables
+from pyparsing import Forward
+from pyparsing import Group
+from pyparsing import nums
+from pyparsing import printables
+from pyparsing import StringEnd
+from pyparsing import StringStart
+from pyparsing import Suppress
+from pyparsing import Word
+from pyparsing import ZeroOrMore
 
+
+def to_int_action(toks, idx=0):
+    """Convert first token to integer"""
+    return int(toks[idx])
 
 # general parsers for all expressions
 exprParser = Forward()
 
-# str parser
-ActualStrParser = Forward()
+
+#################
+# String Parser #
+#################
+# String is formated as:
+# <length of string>:<string>
+
+# Since second part of the parser is using the result from
+# the first part, we are creating a lazy parser and create second part
+# of the parser after we parse the length.
+actualStrParser = Forward()
 
 
+# After first part is parsed, Create second parser with the data
+# from the first part.
 def strParseAction(s, l, t):
     n = t[0]
-    ActualStrParser << Word(printables, exact=n)
+    actualStrParser << Word(printables, exact=n)
+    # since we do not need lenght of the parser anymore
+    # we can ignore it.
     return []
 
+# strLength parser parses length of the string
+# <length of string>:
 strLengthParser = Word(nums) + Suppress(':')
-strLengthParser.setParseAction(lambda t: int(t[0]))
+strLengthParser.setParseAction(to_int_action)
+# after length is parsed create second part of the parser.
 strLengthParser.addParseAction(strParseAction, callDuringTry=True)
-strParser = strLengthParser + ActualStrParser
+# Actual str parser is lenghtParser + ActualStrParser
+strParser = strLengthParser + actualStrParser
 
-# Int Parser
+
+##############
+# Int Parser #
+##############
+# Integer is formated as
+# i<integer>e
 intParser = Suppress('i') + Word(nums) + Suppress('e')
-intParser.setParseAction(lambda t: int(t[0]))
+intParser.setParseAction(to_int_action)
 
-# list Parser
+
+###############
+# List Parser #
+###############
+# List is formated as
+# l[value 1][value2][value3][...]e
 listParser = Group(Suppress('l') + ZeroOrMore(exprParser) + Suppress('e'))
 
 
-exprParser << (intParser | strParser | listParser)
+#####################
+# Dictionary Parser #
+#####################
+# Dictionary is formated as
+# d[key1][value1][key2][value2][...]e
+def dictParseAction(toks):
+    """Convert list of key, value tuples to dictionary"""
+    return [dict(toks.asList())]
 
+dictParser = (Suppress('d') +
+              ZeroOrMore(Group(exprParser + exprParser)) +
+              Suppress('e'))
+dictParser.setParseAction(dictParseAction)
+
+# expr can be int, str, list or dictionary
+exprParser << (intParser | strParser | listParser | dictParser)
+
+# parser that matches the whole string
+parser = StringStart() + exprParser + StringEnd()
+
+
+def decode(st):
+    resp = parser.parseString(st)
+    return resp[0]
 
 if __name__ == '__main__':
-    result = exprParser.parseString('10:12345678901234567890')
-    result = exprParser.parseString('i123e')
-    result = exprParser.parseString('l1:ai123e2:bce')
-    result = exprParser.parseString('l1:ai123e2:bcl1:ai123e2:bcee')
+    result = decode('20:12345678901234567890')
+    result = decode('i123e')
+    result = decode('l1:ai123e2:bce')
+    result = decode('l1:ai123e2:bcl1:ai123e2:bcee')
+    result = decode('d5:apple3:red6:banana6:yellow5:lemon6:yellow6:violet4:bluee')
+
     print(result)
